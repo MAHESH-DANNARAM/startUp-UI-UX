155a765225f4dbd8bbe6948f29673474b344611b
Mahesh <maheshd.mah@gmail.com>
2023-07-17 23:58:20 +0530

    5th

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,51 +1,65 @@
+import { Link } from "react-router-dom";
 import "../components/frontend/assets/css/creative-studio.css";
 import "../components/frontend/assets/vendors/themify-icons/css/themify-icons.css";
 export default function Home() {
   return (
     <>
       <body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
-      <nav className="navbar custom-navbar navbar-expand-lg navbar-dark" data-spy="affix" data-offset-top="20">
-        <div className="container">
+        <nav
+          className="navbar custom-navbar navbar-expand-lg navbar-dark"
+          data-spy="affix"
+          data-offset-top="20"
+        >
+          <div className="container">
             <a className="navbar-brand" href="#">
-                <img src="imgs/logo.png" alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Creative studio Landing page"/>
+              <img
+                src="imgs/logo.png"
+                alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, Creative studio Landing page"
+              />
             </a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span></span>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                        <a className="nav-link" href="#home">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#about">About</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#service">Service</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#portfolio">Portfolio</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#team">Team</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#testimonial">Testimonial</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#blog">Blog</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#contact">Contact</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link btn btn-primary btn-sm ml-lg-3" href="components.html">Components</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+
+          </div>
+        </nav>
 
         <header className="header">
           <div className="overlay">
@@ -399,10 +413,7 @@ export default function Home() {
             <div className="row">
               <div className="col-sm-6 col-md-4">
                 <div className="card text-center mb-4">
-                  <img
-                    className="card-img-top inset"
-                    src="imgs/avatar.jpg"
-                  />
+                  <img className="card-img-top inset" src="imgs/avatar.jpg" />
                   <div className="card-body">
                     <h6 className="small text-primary font-weight-bold">
                       FOUNDER
@@ -431,10 +442,7 @@ export default function Home() {
               </div>
               <div className="col-sm-6 col-md-4">
                 <div className="card text-center mb-4">
-                  <img
-                    className="card-img-top inset"
-                    src="imgs/avatar-1.jpg"
-                  />
+                  <img className="card-img-top inset" src="imgs/avatar-1.jpg" />
                   <div className="card-body">
                     <h6 className="small text-primary font-weight-bold">CEO</h6>
                     <h5 className="card-title">Barbara Ross</h5>
@@ -461,10 +469,7 @@ export default function Home() {
               </div>
               <div className="col-sm-6 col-md-4">
                 <div className="card text-center mb-4">
-                  <img
-                    className="card-img-top inset"
-                    src="imgs/avatar-2.jpg"
-                  />
+                  <img className="card-img-top inset" src="imgs/avatar-2.jpg" />
                   <div className="card-body">
                     <h6 className="small text-primary font-weight-bold">
                       Designer
@@ -493,10 +498,7 @@ export default function Home() {
               </div>
               <div className="col-sm-6 col-md-4">
                 <div className="card text-center mb-4">
-                  <img
-                    className="card-img-top inset"
-                    src="imgs/avatar-3.jpg"
-                  />
+                  <img className="card-img-top inset" src="imgs/avatar-3.jpg" />
                   <div className="card-body">
                     <h6 className="small text-primary font-weight-bold">
                       App Designer
@@ -525,10 +527,7 @@ export default function Home() {
               </div>
               <div className="col-sm-6 col-md-4">
                 <div className="card text-center mb-4">
-                  <img
-                    className="card-img-top inset"
-                    src="imgs/avatar-4.jpg"
-                  />
+                  <img className="card-img-top inset" src="imgs/avatar-4.jpg" />
                   <div className="card-body">
                     <h6 className="small text-primary font-weight-bold">
                       Developer
@@ -557,10 +556,7 @@ export default function Home() {
               </div>
               <div className="col-sm-6 col-md-4">
                 <div className="card text-center mb-4">
-                  <img
-                    className="card-img-top inset"
-                    src="imgs/avatar-5.jpg"
-                  />
+                  <img className="card-img-top inset" src="imgs/avatar-5.jpg" />
                   <div className="card-body">
                     <h6 className="small text-primary font-weight-bold">
                       photographer
