4b58e175bb06c4f2062cb2460bdf628318778349
Abhishek1Mandal <101693497+Abhishek1Mandal@users.noreply.github.com>
2023-08-15 19:49:17 +0530

    Update

---
+++ src/frontend/Login Details/Verification.tsx
@@ -0,0 +1,78 @@
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+} from "mdb-react-ui-kit";
+import "@/assets/frontend/css/login.css";
+import { Link } from "react-router-dom";
+
+function App() {
+  return (
+    <MDBContainer fluid className="p-4">
+      <MDBRow>
+        <MDBCol
+          md="6"
+          className="text-center text-md-start d-flex flex-column justify-content-center"
+        >
+          <h1 className="my-5 display-3 fw-bold ls-tight px-3">
+            The best offer <br />
+            <span className="text-primary">for your business</span>
+          </h1>
+
+          <p className="px-3" style={{ color: "hsl(217, 10%, 50.8%)" }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+            itaque accusantium odio, soluta, corrupti aliquam quibusdam tempora
+            at cupiditate quis eum maiores libero veritatis? Dicta facilis sint
+            aliquid ipsum atque?
+          </p>
+        </MDBCol>
+
+        <MDBCol md="6">
+          <MDBCard className="my-5">
+            <MDBCardBody className="p-5">
+              <div className="grid gap-x-8 gap-y-4 grid-cols-5">
+                <MDBInput
+                  wrapperClass="mb-4"
+                  id="form1"
+                  type="number"
+                />
+                <MDBInput
+                  wrapperClass="mb-4"
+                  id="form1"
+                  type="number"
+                />
+                <MDBInput
+                  wrapperClass="mb-4"
+                  id="form1"
+                  type="number"
+                />
+                <MDBInput
+                  wrapperClass="mb-4"
+                  id="form1"
+                  type="number"
+                />
+                <MDBInput
+                  wrapperClass="mb-4"
+                  id="form1"
+                  type="number"
+                />
+              </div>
+              <Link
+                type="button"
+                to=""
+                className="text-white w-100 mb-4 text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Verify
+              </Link>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  );
+}
+
+export default App;
